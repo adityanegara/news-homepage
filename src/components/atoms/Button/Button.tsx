68a1a4da2196cc2e44fe40fc1980a23156b03778
Aditya Negara <Aditya_N801@mitrais.com>
2022-11-11 11:50:25 +0700

    Refactoring ripple button using react emotion

--- src/components/atoms/Button/Button.tsx
+++ src/components/atoms/Button/Button.tsx
@@ -1,4 +1,6 @@
 import { useState, useEffect, MouseEvent } from "react";
+import styled from "@emotion/styled";
+import { keyframes } from "@emotion/react";
 import "./Button.css";
 
 interface ContainerProps {
@@ -15,6 +17,56 @@ const DEFAULT_COORDINATE = {
   x: -1,
   y: -1,
 };
+
+const rippleEffect = keyframes`
+  0% {
+    transform: scale(1);
+    opacity: 1;
+  }
+  50% {
+    transform: scale(10);
+    opacity: 0.375;
+  }
+  100% {
+    transform: scale(35);
+    opacity: 0;
+  }
+`;
+
+const StyledButton = styled("button")({
+  borderRadius: "4px",
+  border: "none",
+  margin: "8px",
+  padding: "14px 24px",
+  background: "#19762d2",
+  color: "#fff",
+  overflow: "hidden",
+  position: "relative",
+  cursor: "pointer",
+});
+
+const Ripple = styled("span")<Coordinate>(
+  {
+    width: "20px",
+    height: "20px",
+    position: "absolute",
+    background: "coral",
+    display: "block",
+    content: '""',
+    borderRadius: "50%",
+    opacity: 1,
+    animation: `0.9s ease 1 forwards ${rippleEffect}`,
+  },
+  (props) => ({
+    left: props.x,
+    right: props.y,
+  })
+);
+
+const Content = styled("span")({
+  position: "relative",
+  zIndex: 2,
+});
 
 const Button = ({ children, onClick }: ContainerProps): JSX.Element => {
   const [coordinate, setCoordinate] = useState<Coordinate>(DEFAULT_COORDINATE);
@@ -54,14 +106,7 @@ const Button = ({ children, onClick }: ContainerProps): JSX.Element => {
     coordinateX: number,
     coordinateY: number
   ) => {
-    return isRippling ? (
-      <span
-        className="ripple"
-        style={{ left: coordinateX, top: coordinateY }}
-      />
-    ) : (
-      ""
-    );
+    return isRippling ? <Ripple x={coordinateX} y={coordinateY} /> : "";
   };
 
   const handleClick = (e: MouseEvent<HTMLButtonElement>) => {
@@ -70,10 +115,10 @@ const Button = ({ children, onClick }: ContainerProps): JSX.Element => {
     onClick && onClick(e);
   };
   return (
-    <button className="ripple-button" onClick={handleClick}>
+    <StyledButton className="ripple-button" onClick={handleClick}>
       {renderRipple(isRippling, coordinate.x, coordinate.y)}
-      <span className="content">{children}</span>
-    </button>
+      <Content>{children}</Content>
+    </StyledButton>
   );
 };
 
